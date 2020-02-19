89e9f1171c4b2cca75a3ba3d709f110d7661d621
Chris Egan <cegan35@hotmail.com>
2020-02-19 16:19:05 -0500

    Updates

---
+++ precache-manifest.c71fe1d551d5a2c167e068d96547c412.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ad95ca6fc47f0abe99d66acf1794ea",
+    "url": "/simpleapp/index.html"
+  },
+  {
+    "revision": "a98b44c30947f614da35",
+    "url": "/simpleapp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "39b29b021cc446fedc25",
+    "url": "/simpleapp/static/css/main.0cfe433d.chunk.css"
+  },
+  {
+    "revision": "a98b44c30947f614da35",
+    "url": "/simpleapp/static/js/2.be01bc93.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/simpleapp/static/js/2.be01bc93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b29b021cc446fedc25",
+    "url": "/simpleapp/static/js/main.856c8c04.chunk.js"
+  },
+  {
+    "revision": "4bb22878cc9cd86d26cb",
+    "url": "/simpleapp/static/js/runtime-main.43bfccbf.js"
+  },
+  {
+    "revision": "7c43c35755836d3024d8c33035247281",
+    "url": "/simpleapp/static/media/shaggy.7c43c357.png"
+  }
+]);
